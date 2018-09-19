c692103ddab7eeae5efc60234a2ccc1e671957c1
chrisziegler <chrisrziegler@gmail.com>
2018-09-19 14:18:58 -0700

    Added typescript support, updated storybook/addons and storybook/react to v4 alpha, witched to Yarn and yarn-lock for compatibility

---
+++ README.md
@@ -0,0 +1,14 @@
+### Storybook w/React
+
+Template with Welcome page, simple React button component, and Info and JSX addons Start: **> npm run storybook**
+
+Added typescript support
+Appears to require:
+
+- "@storybook/addons": "4.0.0-alpha.3",
+- "@storybook/react": "4.0.0-alpha.3",
+
+(for older version of Webpack without Hooks support (behind the scenes CRA-like build process used by storybook)
+
+- inlined CSS and info for storybook IDE.
+  Switched to Yarn to resolve npm versioning issue, and to install these package dependencies separtely
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { withInfo } from '@storybook/addon-info';
 
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
+import { color } from '@storybook/addon-knobs/react';
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => (
-    <Button bg="palegoldenrod">Hello World</Button>
-  ))
-  .addWithJSX('with background 2', () => (
-    <Button bg="green">Hello World2</Button>
-  ));
+  .addWithJSX(
+    'with background',
+    withInfo({
+      styles: {
+        header: {
+          h1: {
+            color: 'orangered'
+          }
+        }
+      },
+      text: `
+    description of the component
+    ~~~js
+    <Button>Click Here</Button>
+    ~~~
+    `
+    })(() => <Button bg="palegoldenrod">Hello World</Button>)
+  )
+  .addWithJSX(
+    'with background 2',
+    withInfo('description of the text for button 2')(() => (
+      <Button bg={color('bg', 'green', 'group1')}>Hello World2</Button>
+    ))
+  );
